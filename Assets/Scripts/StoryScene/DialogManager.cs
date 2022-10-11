0a58ca909a7099cb3e88d1791ac7ba130ade1d7e
GuanmingLu <40348297+GuanmingLu@users.noreply.github.com>
2022-10-11 17:04:17 +0800

    使用资源加载实例化DialogManager，方便在任何场景直接调用

--- Assets/Scripts/StoryScene/DialogManager.cs
+++ Assets/Scripts/StoryScene/DialogManager.cs
@@ -8,17 +8,34 @@ using EasyTools;
 using NaughtyAttributes;
 using System;
 using Newtonsoft.Json;
+using UnityEditor;
 
 namespace StoryScene {
 
 	public class DialogManager : MonoBehaviour {
+
+		#region 实例化
+
+		private static string _prefabPath = "Assets/Prefab/StoryScene/Dialog.prefab";
+		[RuntimeInitializeOnLoadMethod]
+		private static void InitSelf() {
+			var obj = AssetDatabase.LoadAssetAtPath<GameObject>(_prefabPath);
+			if (obj != null && obj.TryGetComponent<DialogManager>(out _)) {
+				obj = Instantiate(obj);
+				DontDestroyOnLoad(obj);
+				_instance = obj.GetComponent<DialogManager>();
+			}
+			else Debug.LogError("DialogManager初始化失败！请确认 Prefab 路径是否正确！");
+		}
+
+		#endregion
 
 		public static bool Showing { get; private set; } = false;
 
 		private static DialogManager _instance;
 		public static DialogManager Current {
 			get {
-				if (_instance == null) throw new NullReferenceException("DialogManager未实例化！请将预制体置于首个加载的场景中");
+				if (_instance == null) throw new NullReferenceException("DialogManager已被摧毁！");
 				else return _instance;
 			}
 			private set => _instance = value;
@@ -53,12 +70,6 @@ namespace StoryScene {
 		private Queue<DialogMsg> dialogues = new Queue<DialogMsg>();
 
 		private void Awake() {
-			if (_instance != null && _instance != this) Destroy(gameObject);
-			else {
-				_instance = this;
-				DontDestroyOnLoad(gameObject);
-			}
-
 			_panel.SetActive(false);
 			InitAudio();
 		}
