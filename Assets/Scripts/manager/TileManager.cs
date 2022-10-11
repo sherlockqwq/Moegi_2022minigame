c141c50e0883d3ff38de4607be22ff4fd6d5a555
Hongyi.Lan <RetenQ@outlook.com>
2022-10-12 03:56:56 +0800

    增加和拖拽次数相关的UI

--- Assets/Scripts/manager/TileManager.cs
+++ Assets/Scripts/manager/TileManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TileManager : Singleton<TileManager>
 {
 
     public int dragNumInScene;
     [SerializeField] private DragNumber_SO dragNumberData;
+
+    [SerializeField] private Text UI_DragNumber; 
     private void Start()
     {
 
@@ -23,6 +26,17 @@ public class TileManager : Singleton<TileManager>
             Debug.Log("不存在这样的scenename数据");
             dragNumInScene = -1;
         }
+
+        UI_DragNumber = GameObject.Find("UI").transform.Find("DragNumberUI").gameObject.GetComponent<Text>();
         
+    }
+
+    private void Update()
+    {
+        //对于UI的更新我这里先写在Update函数里面了
+        //有必要的话后续记得改
+
+        UI_DragNumber.text = (dragNumInScene-1).ToString();
+        //这里因为拖拽判定的是 > 0 ,所以实际数目需-1
     }
 }
