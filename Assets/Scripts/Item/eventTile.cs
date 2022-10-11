a395697f36a487d80f8737f36f432e804665e877
Hongyi.Lan <RetenQ@outlook.com>
2022-10-12 04:42:37 +0800

    增加到下一关的出口 修改了collection代码，现在为"eventTile.CS",将其添加到对应的tile或预制体 实现收集、重开、到下一关的功能 功能的选择在Inspector中选择

---
+++ Assets/Scripts/Item/eventTile.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum theEvent {  collection, deadArea, exit };
+
+public class eventTile : MonoBehaviour
+{
+    public theEvent choseEvent;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")){
+            GetPoint();
+            if(choseEvent == theEvent.collection)
+            {
+                GetPoint();
+            }
+
+            if (choseEvent == theEvent.deadArea)
+            {
+                deadArea();
+            }
+
+            if (choseEvent == theEvent.exit)
+            {
+                exitScene();
+            }
+        }
+    }
+
+    private void GetPoint()
+    {
+        // 碰到收集品之后触发什么写在这里
+        Debug.Log("得到收藏品");
+        Destroy(gameObject);
+    }
+
+    private void deadArea()
+    {
+        Debug.Log("X area");
+    }
+
+    private void exitScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
