d1f2302de519421b3663fe007a495bb068a11532
SherlockQwQ <73387587+sherlockqwq@users.noreply.github.com>
2022-10-12 16:44:05 +0800

    拖拽时，砖块会变得半透明，并且方块重叠时脱落（吸入动态效果，可参考宇宙小游戏）

--- Assets/Scripts/gridScripts/Tile.cs
+++ Assets/Scripts/gridScripts/Tile.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Tile : MonoBehaviour
 {
-     private Color baseColor, offsetColor;
-     private SpriteRenderer TileRenderer;
+    private Color baseColor, offsetColor;
+    private SpriteRenderer TileRenderer;
     private BoxCollider2D coll;
+    private GameObject overlapTile;
+    public bool isdragging = false;
+    SpriteRenderer targetRenderer;
+    [SerializeField] private float fadeInTime = 0.5f;
+    [SerializeField] private float fadeOutTime = 0.5f;
+
     private void Awake()
     {
         TileRenderer = GetComponent<SpriteRenderer>();
         coll = GetComponent<BoxCollider2D>();
+
+    }
+    private void Start()
+    {
 
     }
     public void Init(bool isoffset)
@@ -18,6 +28,72 @@ public class Tile : MonoBehaviour
 
         /*TileRenderer.color = isoffset ? offsetColor : baseColor;
         */
+    }
+
+    public void FadeIn()
+    {
+
+    }
+    public void FadeOut()
+    {
+        StartCoroutine(FadeOutZoom());
+    }
+    IEnumerator FadeOutZoom()
+    {
+        float time = 0f;
+        while (time < fadeOutTime)
+        {
+            transform.localScale = transform.localScale * Mathf.Lerp(1, 0, time / fadeOutTime);
+
+            time += Time.deltaTime;
+        }
+        yield break;
+    }
+    /*public void DetectTileOverlap()
+    {
+
+
+        var overlapTile = Physics2D.OverlapCircle(transform.position, 0.01f);
+        if (overlapTile != null && overlapTile.CompareTag("Tile") && overlapTile.gameObject.layer == LayerMask.NameToLayer("TileLayer"))
+        {
+            Debug.Log(name + overlapTile.name);
+            targetRenderer = overlapTile.gameObject.GetComponent<SpriteRenderer>();
+            targetRenderer.color = Color.red;
+        }
+        else
+        {
+            targetRenderer.color = Color.green;
+        }
+
+    }*/
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Tile") && isdragging)
+        {
+            Debug.Log(name + "½øÈë" + collision.name + "µÄtrigger");
+            collision.GetComponent<SpriteRenderer>().color = Color.red;
+            overlapTile = collision.gameObject;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Tile") && isdragging)
+        {
+            collision.GetComponent<SpriteRenderer>().color = Color.white;
+            overlapTile = null;
+        }
+    }
+    public void SpriteTranslucent()
+    {
+        TileRenderer.color = new Color(1, 1, 1, 0.5f);
+    }
+    public void SpriteToNormal()
+    {
+        TileRenderer.color = Color.white;
+    }
+    public GameObject getOverlapTile()
+    {
+        return overlapTile;
     }
 
 }
--- Assets/Scripts/manager/GameManager.cs
+++ Assets/Scripts/manager/GameManager.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : Singleton<GameManager>
 {
-    [SerializeField] 
+
+    
     override protected void  Awake()
     {
         base.Awake();
         DontDestroyOnLoad(gameObject);
+    }
+    private void Update()
+    {
+        ReLoadScene();  
+    }
+    void ReLoadScene()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+        }
     }
 }
