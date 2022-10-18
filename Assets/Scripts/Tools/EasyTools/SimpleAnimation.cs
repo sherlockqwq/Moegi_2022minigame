5867b7f288219c90adb3335c0afa35e8b1beebec
GuanmingLu <40348297+GuanmingLu@users.noreply.github.com>
2022-10-18 20:07:44 +0800

    剧情场景2 收集素材 替换完成

---
+++ Assets/Scripts/Tools/EasyTools/SimpleAnimation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EasyTools {
+
+	public class SimpleAnimation : MonoBehaviour {
+		[SerializeField] private Vector3 _moveSpeed, _rotateSpeed;
+		[SerializeField] private Sprite[] _sprites;
+		[SerializeField] private float _frameRate;
+
+		IEnumerator Start() {
+			if (TryGetComponent<SpriteRenderer>(out var sr)) {
+				float interval = 1 / _frameRate;
+				int index = 0;
+				while (true) {
+					sr.sprite = _sprites.ChooseLoop(index++);
+					yield return Wait.Seconds(interval);
+				}
+			}
+		}
+
+		void Update() {
+			transform.Translate(_moveSpeed * Time.deltaTime);
+			transform.Rotate(_rotateSpeed * Time.deltaTime);
+		}
+	}
+}
