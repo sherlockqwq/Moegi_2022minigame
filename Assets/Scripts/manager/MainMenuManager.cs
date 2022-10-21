dd62d3233515c0ab626423276c881ba96d10ca01
GuanmingLu <40348297+GuanmingLu@users.noreply.github.com>
2022-10-21 23:53:58 +0800

    导出设置，项目设置，修复后延时与允许走动，主菜单按键音

--- Assets/Scripts/manager/MainMenuManager.cs
+++ Assets/Scripts/manager/MainMenuManager.cs
@@ -13,26 +13,41 @@ namespace MainMenu {
 	public class MainMenuManager : MonoBehaviour {
 		[SerializeField] private VideoPlayer _vidPlayer;
 		[SerializeField] private RawImage _vidImg;
+		[SerializeField] private GameObject _bg;
+		[SerializeField] private AudioClip _buttonSFX;
 		[SerializeField, Scene] private string _startScene;
 
-		void Awake() {
-			_vidPlayer.loopPointReached += _ => Fade().ApplyTo(this);
-		}
+		private static bool _first = true;
 
-		IEnumerator Fade() {
+		void Awake() {
+			if (_first) {
+				_first = false;
+				_vidPlayer.loopPointReached += _ => VideoAndBgFade().ApplyTo(this);
+				_vidPlayer.Play();
+			}
+			else {
+				_bg.SetActive(true);
+				_vidImg.enabled = false;
+			}
+		}
+		IEnumerator VideoAndBgFade() {
+			_bg.SetActive(true);
 			yield return EasyTools.Gradient.Linear(2f, d => _vidImg.SetA(1 - d));
 			_vidImg.enabled = false;
 		}
 
 		public void StartGame() => EasyGameLoop.Do(StartGameFade());
-
 		IEnumerator StartGameFade() {
+			GameAudio.PlaySFX(_buttonSFX);
 			yield return TransitionManager.Current.ShowMaskCoroutine();
 			yield return SceneManager.LoadSceneAsync(_startScene);
 			yield return TransitionManager.Current.HideMaskCoroutine();
 		}
 
-		public void ExitGame() {
+		public void ExitGame() => EasyGameLoop.Do(ExitGameFade());
+		IEnumerator ExitGameFade() {
+			GameAudio.PlaySFX(_buttonSFX);
+			yield return TransitionManager.Current.ShowMaskCoroutine(1f);
 #if UNITY_EDITOR    //在编辑器模式退出
 			UnityEditor.EditorApplication.isPlaying = false;
 #else//发布后退出
