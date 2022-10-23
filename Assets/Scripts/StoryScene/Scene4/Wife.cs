2f6d5b9fc92022cc28a8f82b9abd986673ff8bd0
GuanmingLu <40348297+GuanmingLu@users.noreply.github.com>
2022-10-23 19:43:10 +0800

    导入剧情场景4素材并进行搭建

---
+++ Assets/Scripts/StoryScene/Scene4/Wife.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StoryScene.Scene4 {
+
+	public class Wife : PlayerInteractable {
+		protected override void OnInteract(StoryPlayerController player) {
+			throw new System.NotImplementedException();
+		}
+	}
+}
