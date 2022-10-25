560828096a13f78465233fab8ac5291c48db17c9
Hongyi.Lan <RetenQ@outlook.com>
2022-10-25 22:27:53 +0800

    合并流程 注意7-8关的BGM不包含在Github上，请自行下载

--- Assets/Scripts/Item/Button_ToNextScene.cs
+++ Assets/Scripts/Item/Button_ToNextScene.cs
@@ -6,8 +6,10 @@ using UnityEngine.SceneManagement;
 public class Button_ToNextScene : MonoBehaviour
 {
     public string nextScene ;  
+    public AudioClip sfx ; 
 
     public void toNextScene(string _sceneName){
+        GameAudio.PlaySFX(sfx);
         SceneManager.LoadScene(_sceneName);
     }
 }
